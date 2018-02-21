6addfede7f9560d402834a75e384fc154bb18574
Nima Seyf <nimaseyf@gmail.com>
2018-02-21 15:21:46 +0000

    currently only first week of month, need every week for ever exercise

--- src/components/ShowWorkout.js
+++ src/components/ShowWorkout.js
@@ -12,24 +12,46 @@ const ShowWorkout = (data) => {
 // When done, remember to come back and implement
 // a recursive solution. Do not repeat code.
 
-const createWeek = (week) => {
-  for(var exercise in week) {
-    console.log(week[exercise])
-  }
+const createSet = (sets) => {
+  return (
+    <div>
+  <p>{sets[1]}</p>
+  <p>{sets[2]}</p>
+  <p>{sets[3]}</p>
+  </div>
+)
 }
 
 
-const createWorkout = (month) => {
-  for(var week in month) {
-    createWeek(month[week]);
+const createWeek = (week) => {
+  for(var exercise in week) {
+    return (
+      <div className="set-block" key={week[exercise]}>
+      {createSet(week[exercise])}
+      </div>
+    )
   }
+}
+
+const createWorkout = (month) => {
+
+    for(var week in month) {
+      console.log(month[week])
+      /* return (
+  <div className="week-block" key={month[week]}>{createWeek(month[week])}</div>
+) */
+  }
+
 }
   // Transform workout object into JSX here
 
   return (
-    <div className="workout-container">{workout.map(createWorkout)}</div>
+    <div className="workout-container">
+    {workout.map(createWorkout)}
+    </div>
   )
 
-
 }
+
+
 export default ShowWorkout;
